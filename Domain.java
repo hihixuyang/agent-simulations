9cadf531726be087ca7a8cce964bc1ae5967a561
Bhavin <bhavingpt@gmail.com>
2017-03-12 20:46:08 -0700

    updated framework

--- Domain.java
+++ Domain.java
@@ -1,4 +1,11 @@
 
-public class Domain {
+public abstract class Domain {
 
+    public abstract void visualize();
+
+    public abstract void startSim(int iterations);
+
+    public abstract void nextTurn();
+
+    public abstract void initializeAgents(Agent[] agents);
 }
--- TestDomain.java
+++ TestDomain.java
@@ -1,3 +1,4 @@
 
 public class TestDomain extends Domain {
+
 }
