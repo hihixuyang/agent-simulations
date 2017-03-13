82223efd94e3d12e5666ae32c7e6a67b9164bf77
Bhavin <bhavingpt@gmail.com>
2017-03-12 20:24:19 -0700

    basic framework

---
+++ Simulator.java
@@ -0,0 +1,18 @@
+
+public class Simulator {
+
+    private Domain dom;
+
+    public Simulator() {
+        dom = new TestDomain();
+    }
+
+    public void runSim() {
+
+    }
+
+    public static void main(String[] args) {
+        Simulator prog = new Simulator();
+        prog.runSim();
+    }
+}
