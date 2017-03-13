79843d769fb72c0aee0f0b2b556fcd5fba56a076
Bhavin <bhavingpt@gmail.com>
2017-03-12 20:59:50 -0700

    improved framework

--- Domain.java
+++ Domain.java
@@ -1,11 +1,38 @@
 
 public abstract class Domain {
 
+    /**
+     * This is a method that every domain class should implement, which
+     * allows the programmer to visualize the current state of the domain,
+     * including any and all relevant information like positions, etc.
+     */
     public abstract void visualize();
 
+    /**
+     * Used by the Simulator class (which is the main class) to start the simulation.
+     * @param iterations The number of iterations for which the simulation should execute
+     */
     public abstract void startSim(int iterations);
 
+    /**
+     * This will be called mostly from within the domain class, but let's require that
+     * every subclass of Domain implement it in order to standardize the implementation if possible.
+     */
     public abstract void nextTurn();
 
+    /**
+     * This will be called by the Simulator, which passes in an array of agents which will all be
+     * the ones participating in the simulation. Domain can then call getAction() and update these
+     * agents directly from this array.
+     * @param agents An array of all the agents that will participate in the simulation.
+     */
     public abstract void initializeAgents(Agent[] agents);
+
+    /**
+     * Called at the very end of the Simulator, once the simulation has finished executing. This will return
+     * an integer value, possibly out of 1000, that describes the 'success rate' of the simulation for an easy
+     * visualization / later analysis.
+     * @return the success rate of the simulation, in some standardized form
+     */
+    public abstract int getSuccessRate();
 }
--- Simulator.java
+++ Simulator.java
@@ -2,17 +2,45 @@
 public class Simulator {
 
     private Domain dom;
+    private int iterations;
 
-    public Simulator() {
-        dom = new TestDomain();
+    private Simulator(Domain d, int iter) {
+        dom = d;
+        iterations = iter;
     }
 
-    public void runSim() {
-
+    /**
+     * Responsible for initializing the agents that will be used in the simulation.
+     * This should be called through the Simulator() constructor so the user can customize
+     * this as desired from the main() method.
+     * @return an array of Agents, each possibly a separate subclass, describing the agents
+     */
+    private Agent[] initializeAgents() {
+        return null;
     }
 
+    /**
+     * Performs the main function of the simulator - passes the domain the agents and
+     * begins the simulation. It also retrieves the completion rate at the very end.
+     */
+    private int execute() {
+        Agent[] agents = initializeAgents();
+        dom.initializeAgents(agents);
+        dom.startSim(iterations);
+
+        return dom.getSuccessRate();
+    }
+
+    /**
+     * The main method. Simply creates a new simulator and lets execute() do the work.
+     */
     public static void main(String[] args) {
-        Simulator prog = new Simulator();
-        prog.runSim();
+
+        Domain d = new TestDomain();
+
+        Simulator prog = new Simulator(d, 1000);
+        int result = prog.execute();
+
+        System.out.println(result);
     }
 }
